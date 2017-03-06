mono/mono-addins

commit 47df1d4b62c5514ed23cb93e6ca8a298a21ca90f

Fix flicker showing Add-in Manager
Don't show the dialog before its layout happens.

RUnning will show it.

Mono.Addins.Gui/Mono.Addins.Gui/AddinManagerDialog.cs
@@ -72,7 +72,6 @@ namespace Mono.Addins.Gui
 			TransientFor = parent;
 			HasSeparator = false;
 			Services.PlaceDialog (this, parent);
-			Show ();
 			
 			addininfoInstalled.Init (service);
 			addininfoGallery.Init (service);

Mono.Addins.GuiGtk3/Mono.Addins.Gui/AddinManagerDialog.cs
@@ -118,7 +118,6 @@ namespace Mono.Addins.GuiGtk3
 			eventbox3.Child = addininfoUpdates;
 
 //			Services.PlaceDialog (this, parent);
-			Show ();
 			
 			addininfoInstalled.Init (service);
 			addininfoGallery.Init (service);

commit 691bdb6604155e9b18a038382b527ae09d995f43

Fixed search box in AddInManager not appearing This regression was introduced with 47df1d4

Mono.Addins.Gui/Mono.Addins.Gui/AddinManagerDialog.cs
@@ -161,7 +161,6 @@ namespace Mono.Addins.Gui
 		{
 			filterEntry = new SearchEntry ();
 			filterEntry.Entry.SetSizeRequest (200, filterEntry.Entry.SizeRequest ().Height);
-			filterEntry.Parent = notebook;
 			filterEntry.Show ();
 			notebook.SizeAllocated += delegate {
 				RepositionFilter ();
@@ -175,6 +174,12 @@ namespace Mono.Addins.Gui
 				galleryTreeView.ExpandAll ();
 			};
 			RepositionFilter ();
+		}
+
+		protected override void OnShown ()
+		{
+			base.OnShown ();
+			filterEntry.Parent = notebook;
 		}
 		
 		void RepositionFilter ()

Mono.Addins.GuiGtk3/Mono.Addins.Gui/AddinManagerDialog.cs
@@ -216,7 +216,6 @@ namespace Mono.Addins.GuiGtk3
 		{
 			filterEntry = new SearchEntry ();
 			filterEntry.Entry.SetSizeRequest (200, filterEntry.Entry.SizeRequest ().Height);
-			filterEntry.Parent = notebook;
 			filterEntry.Show ();
 			notebook.SizeAllocated += delegate {
 				RepositionFilter ();
@@ -238,6 +237,12 @@ namespace Mono.Addins.GuiGtk3
 			int h = filterEntry.SizeRequest ().Height;
 			var alloc = notebook.Allocation;
 			filterEntry.SetAllocation (new Gdk.Rectangle (alloc.Left + alloc.Width - 1 - w, alloc.Y, w, h));
+		}
+
+		protected override void OnShown ()
+		{
+			base.OnShown ();
+			filterEntry.Parent = notebook;
 		}
 		
 //		public override void Dispose ()